bea281ef3dab67e25c7f4bd0a54b3cb770c24bf2
Rodney <github@rodneyfolz.com>
2021-10-23 12:29:55 -0700

    Disable some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,16 @@ module.exports = {
     project: "./tsconfig.json",
   },
   rules: {
+    "prefer-arrow-callback": "off",
     "import/no-default-export": "error",
     "import/prefer-default-export": "off",
     "react/require-default-props": "off",
+    "react/prop-types": "off",
     "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
+    "@typescript-eslint/lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
   },
 };
